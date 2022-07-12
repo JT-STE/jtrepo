56329b59dff5b3e30b734c70d90967b2d0259523
Jerry Tang <tang.zhenxing.jerry@stengg.com>
2022-07-12 23:39:06 +0800

    update

--- workshop01/index.js
+++ workshop01/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 4000;
 
 //Loads the handlebars module
 const handlebars = require('express-handlebars');
